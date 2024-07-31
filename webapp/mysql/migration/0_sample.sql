e84447a948446ee93274bfb820d27e9427b95077
kitsuki <IT.King5963@gmail.com>
2024-07-31 19:43:26 +0900

    add index

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -7,3 +7,14 @@ CREATE INDEX node_id ON orders
 
 CREATE INDEX area_id ON nodes
 CREATE INDEX id ON nodes
+
+CREATE INDEX status ON tow_trucks
+CREATE INDEX area_id ON tow_trucks
+CREATE INDEX driver_id ON tow_trucks
+CREATE INDEX id ON tow_trucks
+
+CREATE INDEX id ON users
+
+CREATE INDEX tow_truck_id ON locations
+CREATE INDEX timestamp ON locations
+
